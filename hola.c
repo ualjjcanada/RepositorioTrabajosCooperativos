7fa740bdf49a2bd70264e552a50f4ab71976d442
Isa del Águila <37574770+imaguila@users.noreply.github.com>
2021-11-09 10:39:10 +0100

    Update hola.c

--- hola.c
+++ hola.c
@@ -4,9 +4,5 @@
 int main(){
 	printf(Hola.\n");
 	printf("Este es mi primer programa\n");
-	printf("escrito en C de la carrera.\n");
-	printf("Adios.\n");
-	printf("\n\n\nPulse una tecla para finalizar");
-	getchar();
-	return 0;
+
 }
