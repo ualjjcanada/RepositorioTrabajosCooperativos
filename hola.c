1ae981db15d6856fa52697bc1c6be5208465be9e
Isa del Águila <37574770+imaguila@users.noreply.github.com>
2021-11-09 10:01:22 +0100

    Create hola.c

---
+++ hola.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+/* Programa inicial */
+int main(){
+	printf(Hola.\n");
+	printf("Este es mi primer programa\n");
+	printf("escrito en C de la carrera.\n");
+	printf("Adios.\n");
+	printf("\n\n\nPulse una tecla para finalizar");
+	getch();
+	return 0;
+}
